428d0ca24fd00271b63d1736ed2760a22dadc1bc
Andy Biewer <andrew.biewer@ge.com>
2018-03-14 09:53:27 -0500

    Add week 8 materials

---
+++ week8/projects/NamesApp8/src/main/java/edu/wctc/dj/week8/namesapp8/beans/CartBean.java
@@ -0,0 +1,35 @@
+package edu.wctc.dj.week8.namesapp8.beans;
+
+import edu.wctc.dj.week8.namesapp8.model.Name;
+import edu.wctc.dj.week8.namesapp8.model.ShoppingCart;
+import edu.wctc.dj.week8.namesapp8.model.ShoppingCartService;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
+@Named(value = "cartBean")
+@SessionScoped
+public class CartBean implements Serializable {
+
+	private final String sessionId;
+	private final ShoppingCart cart;
+	private final ShoppingCartService cartService = new ShoppingCartService();
+
+	public CartBean() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		sessionId = facesContext.getExternalContext().getSessionId(true);
+		cart = cartService.getContents(sessionId);
+	}
+
+	public int getItemsInCart() {
+		return cart.getItemsInCart();
+	}
+
+	public void addToCart(Name name) {
+		cart.add(name);
+		cartService.update(sessionId, cart);
+	}
+
+	
+}
