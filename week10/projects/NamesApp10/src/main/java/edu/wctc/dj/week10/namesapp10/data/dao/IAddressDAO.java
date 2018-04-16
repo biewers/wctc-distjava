f23aea079397d3dbae505bc9c3e9dd075d40cb4e
Andy Biewer <andrew.biewer@ge.com>
2018-04-15 19:42:39 -0500

    Commit week 10 work

---
+++ week10/projects/NamesApp10/src/main/java/edu/wctc/dj/week10/namesapp10/data/dao/IAddressDAO.java
@@ -0,0 +1,9 @@
+package edu.wctc.dj.week10.namesapp10.data.dao;
+
+import edu.wctc.dj.week10.namesapp10.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAddressDAO extends JpaRepository<Address, String>
+{
+	
+}
---
+++ week10/projects/NamesApp10/src/main/java/edu/wctc/dj/week10/namesapp10/data/dao/INameDAO.java
@@ -0,0 +1,9 @@
+package edu.wctc.dj.week10.namesapp10.data.dao;
+
+import edu.wctc.dj.week10.namesapp10.model.Name;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface INameDAO extends JpaRepository<Name, String>
+{
+	
+}
